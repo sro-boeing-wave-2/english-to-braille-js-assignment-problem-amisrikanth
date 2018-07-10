9442364d2a2b5f30f0806550839f32b75d833992
Srikanth Banagadi <amisrikanth@gmail.com>
2018-07-10 16:48:00 +0530

    finished the assignment

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+const brailleMap = new Map(englishToBrailleLiteralSet);
+function convertFunction() {
+  const str = document.getElementById('sourceLangText').value;
+  const resultArray = [];
+  for (let i = 0; i < str.length; i += 1) {
+    resultArray.push(brailleMap.get(str[i]));
+  }
+  document.getElementById('targetLangText').innerHTML = resultArray.join('');
+}
+document.getElementById('btnConvertEnglishToBraille').addEventListener('click', convertFunction);
